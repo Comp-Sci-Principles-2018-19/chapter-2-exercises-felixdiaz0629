979c07bb8dcefd9f93dd05b644a8234ba946854e
Felix <fdiaz2021@kewforest.org>
2018-10-05 08:14:49 -0400

    Error Message

--- Sentence.py
+++ Sentence.py
@@ -3,9 +3,13 @@ w2="work"
 w3="and"
 w4="no"
 w5="play"
-w6="makes"
+#w6="makes"
 w7="Jack"
 w8="a"
 w9="dull"
 w10="boy"
 print(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10)
+#Traceback (most recent call last):
+  #File "/Users/FelixDiaz/Documents/GitHub/chapter-2-exercises-felixdiaz0629/Sentence.py", line 11, in <module>
+    #print(w1,w2,w3,w4,w5,w6,w7,w8,w9,w10)
+#NameError: name 'w6' is not defined
